292510ee2261203fd0ef3fc55f6efcfed995dd71
Ragava Krishnan <210701201@rajalakshmi.edu.in>
2024-11-08 09:48:56 +0530

    feat: update java solution for group-anagrams

--- group-anagrams/group-anagrams.java
+++ group-anagrams/group-anagrams.java
@@ -2,13 +2,15 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> map = new HashMap<>();
         for(String str : strs){
-            char[] chars = str.toCharArray();
-            Arrays.sort(chars);
-            String key = new String(chars);
-            if(!map.containsKey(key)){
-                map.put(key, new ArrayList<>());
-                map.get(key).add(str);
+            int[] count = new int[26];
+            for(int i=0;i<str.length();i++){
+                char c = str.charAt(i);
+                count[c - 'a']++;
             }
+
+            String key = Arrays.toString(count);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
         }
         return new ArrayList<>(map.values());
     }
