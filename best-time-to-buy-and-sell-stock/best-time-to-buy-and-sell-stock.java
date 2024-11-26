bc11de8d419c8aeddfb911e770ba9b3573a019b9
Ragava Krishnan <210701201@rajalakshmi.edu.in>
2024-11-27 00:47:01 +0530

    feat: update java solution for best-time-to-buy-and-sell-stock

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int maxProfit = 0;
-        int minPrice = prices[0];
+        int minPrice = Integer.MAX_VALUE;
         for(int currentPrice : prices){
             minPrice = Math.min(currentPrice, minPrice);
             maxProfit = Math.max(currentPrice - minPrice, maxProfit);
