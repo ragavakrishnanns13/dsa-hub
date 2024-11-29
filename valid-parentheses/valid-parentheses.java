3284158e2005381d3d33dc5c01a668fc156c2e2f
Ragava Krishnan <210701201@rajalakshmi.edu.in>
2024-11-29 15:48:44 +0530

    feat: update java solution for valid-parentheses

--- valid-parentheses/valid-parentheses.java
+++ valid-parentheses/valid-parentheses.java
@@ -1,10 +1,20 @@
-public class Solution {
+class Solution {
     public boolean isValid(String s) {
-        while (s.contains("()") || s.contains("{}") || s.contains("[]")) {
-            s = s.replace("()", "");
-            s = s.replace("{}", "");
-            s = s.replace("[]", "");
+        Stack<Character> st = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                st.push(c);
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char top = st.pop();
+                if((c == ')' && top != '(') || (c == '}' && top != '{') || c == ']' && top != '['){
+                    return false;
+                }
+            }
         }
-        return s.isEmpty();
+        return st.isEmpty();
     }
 }
