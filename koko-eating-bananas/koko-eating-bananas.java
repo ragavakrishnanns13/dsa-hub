f10a4e006fefd3d84c5616a2d01ed2e5d14437a4
Ragava Krishnan <210701201@rajalakshmi.edu.in>
2025-02-07 18:22:47 +0530

    feat: add java solution for koko-eating-bananas

---
+++ koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1, right;
+        for(int pile : piles){
+            right = Math.max(right, pile);
+        }
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (f(piles, mid) <= h) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+    
+    private int f(int[] piles, int speed) {
+        int hours = 0;
+        for (int i = 0; i < piles.length; i++) {
+            hours += piles[i] / speed;
+            if (piles[i] % speed > 0) hours++;
+        }
+        return hours;
+    }
+}
